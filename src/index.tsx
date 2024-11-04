1eef975084cf4b07923a37987e02202f2d137412
康昆 <bookmak@163.com>
2024-11-04 22:48:48 +0800

    react测试

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,16 @@
-let a = 1;
-let b = 2;
-console.log(a + b);
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("app");
+
+if (!container) {
+  throw new Error("Failed to find the root element");
+}
+
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <div>React Hello </div>
+  </React.StrictMode>
+);
