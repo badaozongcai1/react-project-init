fd3174cbd739e94d1858af82a63e3607d670bbdc
badao <0xbadao1@proton.me>
2024-12-10 13:59:02 +0800

    ts usage test6

---
+++ src/utils/DeepReadonly.ts
@@ -0,0 +1,31 @@
+// Implement a generic DeepReadonly<T> which make every parameter of an object - and its sub-objects recursively - readonly.
+
+// You can assume that we are only dealing with Objects in this challenge. Arrays, Functions, Classes and so on do not need to be taken into consideration. However, you can still challenge yourself by covering as many different cases as possible.
+
+// For example:
+
+type X = {
+  x: {
+    a: 1;
+
+    b: "hi";
+  };
+
+  y: "hey";
+};
+type DeepReadonly<T> = {
+  readonly [key in keyof T]: T[key] extends object
+    ? DeepReadonly<T[key]>
+    : T[key];
+};
+type Expected = {
+  readonly x: {
+    readonly a: 1;
+
+    readonly b: "hi";
+  };
+
+  readonly y: "hey";
+};
+
+type Todo = DeepReadonly<X>; // should be same as `Expected`
